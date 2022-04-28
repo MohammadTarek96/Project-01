99f308a74eecc9c580ed31d4a1d311d0fb143f06
MohammadTarek96 <capjoh@gmail.com>
2022-04-28 17:16:27 +0100

    1-applied some linting to Start, and positioned the text correctly in Donate.

--- Project 01/game/Donate.py
+++ Project 01/game/Donate.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from PIL import ImageTk, Image
+import tkinter.font as tkfont
+import textwrap
 import os
 
 
@@ -13,19 +15,17 @@ class Donate(Frame):
 
         background_label = Label(self, image=self.img)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
-        text = Label(self, text="we thank you for playing our game there are a lot of homless people", bg='black',
-                     fg='white')
-        text2 = Label(self, text="who have to go through the struglles you experinced in the game in real life.",
-                      bg='black', fg='white')
-        text3 = Label(self, text="Please help them by donating to National Alliance to End Homelessness", bg='black',
-                      fg='white')
-        text.place(x=20, y=80)
-        text.config(font=('Sail bold',20))
-        text2.place(x=30, y=120)
-        text2.config(font=('Sail bold',20))
-        text3.place(x=40, y=160)
-        text3.config(font=('Sail bold',20))
+
+        font = tkfont.Font(family="Times New Roman", size=16, weight="bold")
+
+        text = textwrap.fill('we thank you for playing our game there are a lot of homless people '
+                             'who have to go through the struglles you experinced in the game in real life.'
+                             'Please help them by donating to National Alliance to End Homelessness.', )
+
+        text = Label(self, width=50, text=text)
+        text.configure(font=font)
+        text.place(relx=0.5, rely=0.3, anchor=CENTER)
 
         btn_about = Button(self, text="Go Back", bg='black', fg='white',
                            command=lambda: self.controller.show_frame('Home'))
-        btn_about.place(relx=0.5, rely=0.6, width=150, height=50, anchor=CENTER)
+        btn_about.place(relx=0.5, rely=0.5, width=150, height=50, anchor=CENTER)
--- Project 01/game/Start.py
+++ Project 01/game/Start.py
@@ -4,6 +4,7 @@ from PIL import ImageTk, Image
 import os
 import textwrap
 
+
 class Start(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
@@ -15,34 +16,46 @@ class Start(Frame):
         background_label = Label(self, image=self.img)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        my_font = tkfont.Font(family="Times New Roman", size=20, weight="bold" )  # slant="italic"
+        my_font = tkfont.Font(family="Times New Roman", size=16, weight="bold")  # slant="italic"
 
-        textFont = tkfont.Font(family="Times New Roman", size=10, weight= "bold" )
+        font = tkfont.Font(family="Times New Roman", size=10, weight="bold")
 
         text = Label(self, text='Please choose a character')
         text.configure(font=my_font)
         text.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-        self.btn_woman_bg = ImageTk.PhotoImage(Image.open(os.path.join(self.controller.dirname, '../Assets/btn_woman.jpg')))
-        img_label= Label(image=self.btn_woman_bg)
+        self.btn_woman_bg = ImageTk.PhotoImage(
+            Image.open(os.path.join(self.controller.dirname, '../Assets/btn_woman.jpg')))
+        img_label = Label(image=self.btn_woman_bg)
 
-        self.btn_man_bg = ImageTk.PhotoImage(Image.open(os.path.join(self.controller.dirname, '../Assets/homeless-man.jpg')))
-        img_label= Label(image=self.btn_man_bg)
+        self.btn_man_bg = ImageTk.PhotoImage(
+            Image.open(os.path.join(self.controller.dirname, '../Assets/homeless-man.jpg')))
+        img_label = Label(image=self.btn_man_bg)
 
-        btn_male   = Button(self, image=self.btn_man_bg)
+        btn_male = Button(self, image=self.btn_man_bg)
         btn_female = Button(self, image=self.btn_woman_bg)
-        btn_back   = Button(self, text="Go Back", bg='black', fg='white', command=lambda: self.controller.show_frame('Home'))
+        btn_back = Button(self, text="Go Back", bg='black', fg='white',
+                          command=lambda: self.controller.show_frame('Home'))
 
-        btn_male.place(  relx=0.75, rely=0.35, width=256, height=256, anchor=CENTER)
+        btn_male.place(relx=0.75, rely=0.35, width=256, height=256, anchor=CENTER)
         btn_female.place(relx=0.25, rely=0.35, width=256, height=256, anchor=CENTER)
-        btn_back.place(  relx=0.5,  rely=0.9,  width=150, height=50,  anchor=CENTER)
+        btn_back.place(relx=0.5, rely=0.9, width=150, height=50, anchor=CENTER)
 
-        text_female = textwrap.fill('This is Lisa. She used to work as a preschool teacher. After she lost both of her children, she got depressed and lost everything she had. Do you want to help Lisa get back on track?', width=50)
+        text_female = textwrap.fill(
+            'This is Lisa. She used to work as a preschool teacher. '
+            'After she lost both of her children, she got depressed and lost everything she had. '
+            'Do you want to help Lisa get back on track?',
+            width=50)
+
         text_female = Label(self, width=50, text=text_female)
-        text_female.configure(font=textFont)
+        text_female.configure(font=font)
         text_female.place(relx=0.25, rely=0.6, anchor=CENTER)
 
-        text_male = textwrap.fill('This is Edward. He fought hard for his country. But the PTSD after the things he has been through wouldnt leave him alone. He needs your help to get his house back. Will you help him?', width=50)
+        text_male = textwrap.fill(
+            'This is Edward. He fought hard for his country. '
+            "But the PTSD after the things he has been through wouldn't leave him alone. "
+            'He needs your help to get his house back. Will you help him?',
+            width=50)
         text_male = Label(self, width=50, text=text_male)
-        text_male.configure(font=textFont)
+        text_male.configure(font=font)
         text_male.place(relx=0.75, rely=0.6, anchor=CENTER)
