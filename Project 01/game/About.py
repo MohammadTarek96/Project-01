0e2e63ead28c435bdc9eed5a3e297e597968a775
ahmadelj <ahmad.eljachi98@gmail.com>
2022-04-28 17:16:40 +0200

    Text about the game on About

--- Project 01/game/About.py
+++ Project 01/game/About.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.font as tkfont
 from PIL import ImageTk, Image
 import os
 
@@ -13,6 +14,11 @@ class About(Frame):
 
         background_label = Label(self, image=self.img)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        textAboutGame = Label(self, text="we thank you for playing our game there are a lot of homless people", bg='black',
+                     fg='white')
+        aboutGameFont = tkfont.Font(family="Times New Roman", size=16, weight="bold" )  # slant="italic"
+        textAboutGame.configure(font=aboutGameFont)
+        textAboutGame.place(relx=0.5, rely=0.1, anchor=CENTER)
 
         btn_about = Button(self, text="Go Back", bg='black', fg='white',
                            command=lambda: self.controller.show_frame('Home'))
