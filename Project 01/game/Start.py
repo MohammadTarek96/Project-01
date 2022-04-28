a605699f8c9fd7b53287c208dd060c11dcfb77d3
MohammadTarek96 <capjoh@gmail.com>
2022-04-28 09:01:11 +0100

    1- Added 2 click for (Male-Female) Character selection after the start button. Note-at the moment they both function as Go back button. 2- Added 'Play as' text(Font-Times new Roman, Size-20).

--- Project 01/game/Start.py
+++ Project 01/game/Start.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.font as tkfont
 from PIL import ImageTk, Image
 import os
 
@@ -14,6 +15,22 @@ class Start(Frame):
         background_label = Label(self, image=self.img)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+        my_font = tkfont.Font(family="Times New Roman", size=20, weight="bold", )  # slant="italic"
+
+        text = Label(self, text='Play as', bg='black', fg='white')
+
+        text.configure(font=my_font)
+
+        text.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+        btn_start = Button(self, text='Male', bg='black', fg='white',
+                           command=lambda: self.controller.show_frame('Home'))
+        btn_start.place(relx=0.5, rely=0.5, width=150, height=50, anchor=CENTER)
+
+        btn_start = Button(self, text='Female', bg='black', fg='white',
+                           command=lambda: self.controller.show_frame('Home'))
+        btn_start.place(relx=0.5, rely=0.4, width=150, height=50, anchor=CENTER)
+
         btn_start = Button(self, text="Go Back", bg='black', fg='white',
                            command=lambda: self.controller.show_frame('Home'))
-        btn_start.place(relx=0.5, rely=0.3, width=150, height=50, anchor=CENTER)
+        btn_start.place(relx=0.5, rely=0.6, width=150, height=50, anchor=CENTER)
